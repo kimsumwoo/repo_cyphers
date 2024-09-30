94dbff11f23fdd0fb624af31f956f702f778b820
kimsunwoo <tjsdn3031@gmail.com>
2024-09-30 17:30:26 +0900

    544

--- cyphers/src/main/java/com/loras/infra/login/LogInController.java
+++ cyphers/src/main/java/com/loras/infra/login/LogInController.java
@@ -1,9 +1,16 @@
 package com.loras.infra.login;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.loras.infra.member.MemberDto;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class LogInController {
@@ -18,5 +25,20 @@ public class LogInController {
 	public String helloworld(LogInDto logInDto) {
 		logInService.logInSelectOne();
 		return "/xdm/v1/infra/login/helloworld";
+	}
+	@ResponseBody
+	@RequestMapping(value = "/xdm/v1/infra/login/signinXdmProc")
+	public Map<String, Object> signinXdmProc(LogInDto logInDto) throws Exception {
+		System.out.println("sadsadsad");
+		Map<String, Object> returnMap = new HashMap<String, Object>();
+		
+		LogInDto rtMember2 = logInService.logInSelectOne(); 
+
+		if (rtMember2 != null) {
+			returnMap.put("rt", "success");
+		} else {
+			returnMap.put("rt", "fail"); 
+		}
+		return returnMap;
 	}
 }
